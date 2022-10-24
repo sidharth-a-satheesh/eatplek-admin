18529884aa4cd62a6ca63ebde74e1951a62b564d
Arshad Danish <arshaddanishthana@gmail.com>
2022-10-24 13:47:39 +0530

    up

--- src/pages/revenue/Revenue.js
+++ src/pages/revenue/Revenue.js
@@ -11,14 +11,26 @@ const Revenue = () => {
   let onFormSubmit = async (e) => {
     e.preventDefault();
     setSuccess(true);
-    let res = await apis.post("admin/revenue", formData, {
-      headers: {
-        "Content-Type": "application/json",
-        Token: localStorage.getItem("jwt_admin"),
+    let res = await apis.post(
+      "admin/revenue",
+      {
+        start_date: new Date(formData.start_date).toISOString(),
+        end_date: new Date(formData.end_date).toISOString(),
       },
-    });
-    console.log(res);
-    setData(res);
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Token: localStorage.getItem("jwt_admin"),
+        },
+      }
+    );
+    console.log(res.data);
+    setData(res.data);
+    setSuccess(false);
+  };
+
+  let onInputChange = async (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   return (
@@ -33,6 +45,8 @@ const Revenue = () => {
             name="start_date"
             min="2022-08-06"
             max="2040-12-31"
+            onChange={onInputChange}
+            required
           ></input>
           <label className="end-date-label" for="end">
             End date:
@@ -43,31 +57,34 @@ const Revenue = () => {
             name="end_date"
             min="2022-08-06"
             max="2040-12-31"
+            onChange={onInputChange}
+            required
           ></input>
           <Button variant="contained" type="submit">
             {success ? "Submitting..." : "Submit"}
           </Button>
         </div>
       </form>
-      <h1 className="revenue-text-right">Total Revenue: ₹50,000</h1>
+      <h1 className="revenue-text-right">Total Revenue: {data.Total}</h1>
       <div className="revenue-hotel-heading">
         <div>Sl. No.</div>
         <div>Hotel Name</div>
         <div>Revenue</div>
       </div>
       <hr className="revenue-hr" />
-      {data.Restaurants.map((e, i) => {
-        return (
-          <>
-            <div className="revenue-hotel">
-              <div>1</div>
-              <div>The Smocky Shack</div>
-              <div>₹5,000</div>
-            </div>
-            <hr className="revenue-hr" />
-          </>
-        );
-      })}
+      {data.Restaurants &&
+        data.Restaurants.map((e, i) => {
+          return (
+            <>
+              <div className="revenue-hotel">
+                <div>{i+1}</div>
+                <div>{e.Name}</div>
+                <div>₹{e.Revenue}</div>
+              </div>
+              <hr className="revenue-hr" />
+            </>
+          );
+        })}
     </div>
   );
 };
